1005dd98250b1de81fa957baf1496a28a334c445
xinxin321198 <362527240@qq.com>
2023-10-25 16:04:28 +0800

    农民知识竞赛的脚本

---
+++ AutoCompetitionOrdinary.js
@@ -0,0 +1,52 @@
+// ==UserScript==
+// @name         loserStar老王农民知识竞赛
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://nongminjingsai.com/phone/exam/commonExam
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=tampermonkey.net
+// @grant        none
+// @require      https://cdn.bootcdn.net/ajax/libs/jquery/3.7.1/jquery.js
+// ==/UserScript==
+this.$ = this.jQuery = jQuery.noConflict(true);
+
+(function() {
+    'use strict';
+    setInterval(function(){
+        console.log("获取包含选项的div");
+        var itemList = $(".item");
+        if (itemList.length>0){
+            var item = itemList[0];
+            console.log("获取div下的所有选项");
+            var children = $(item).children();
+            var selectedIndex = Math.floor(Math.random() * children.length);
+            console.log("点击第" + (selectedIndex+1) +"个选项");
+            $(children[selectedIndex]).trigger("click");
+            console.log("获取下一题按钮");
+
+            //如果出现继续答题，就选择继续答题
+            var jixudatiBtn = $(".van-button.van-button--default.van-button--large.van-button--round");
+            if(jixudatiBtn.length>0){
+                console.log("点击继续答题");
+                $(jixudatiBtn[1]).trigger("click");
+            }else{
+                //如果出现提交按钮，就点击提交，不点击下一题
+                var tijiaoBtn = $(".van-button.van-button--default.van-button--normal");
+                if (tijiaoBtn.length > 0) {
+                    console.log("点击提交");
+                    $(tijiaoBtn).trigger("click");
+                } else {
+                    var xiayitiBtn = $("span:contains(下一题)");
+                    console.log("点击下一题按钮");
+                    $(xiayitiBtn).trigger("click");
+                }
+            }
+
+
+
+        }
+    },1000);
+
+
+})();
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # loserStarTampermonkey
-这是自己编写的篡改猴的脚本合集
+## 这是自己编写的篡改猴的脚本合集
+### AutoCompetitionOrdinary.js：老王的农民知识竞赛，普通答题的自动脚本https://nongminjingsai.com/phone/login?redirect=%2Fexam%2FcommonExam%3F
